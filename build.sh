59a804e912db1ee8f62b509c9f5eafe44dbbbb4e
paper2017 <paper2017anonym@yahoo.com>
2016-10-18 22:45:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,25 +4,22 @@ VERSION=$1
 
 if [ "$VERSION"V == SF1V ];then
     echo building for slim-fat sketch version 1
-    cmake -DSF1=ON -DSF2=OFF -DSF3=OFF -DSF4=OFF -DSF5=OFF -DSF6=OFF .
+    cmake -DSF1=ON -DSF2=OFF -DSF3=OFF -DSF4=OFF -DSFF=OFF .
 elif [ "$VERSION"V == SF2V ];then
     echo building for slim-fat sketch version 2
-    cmake -DSF1=OFF -DSF2=ON -DSF3=OFF -DSF4=OFF -DSF5=OFF -DSF6=OFF .
+    cmake -DSF1=OFF -DSF2=ON -DSF3=OFF -DSF4=OFF -DSFF=OFF .
 elif [ "$VERSION"V == SF3V ];then
     echo building for slim-fat sketch version 3
-    cmake -DSF1=OFF -DSF2=OFF -DSF3=ON -DSF4=OFF -DSF5=OFF -DSF6=OFF .
+    cmake -DSF1=OFF -DSF2=OFF -DSF3=ON -DSF4=OFF -DSFF=OFF .
 elif [ "$VERSION"V == SF4V ];then
     echo building for slim-fat sketch version 4
-    cmake -DSF1=OFF -DSF2=OFF -DSF3=OFF -DSF4=ON -DSF5=OFF -DSF6=OFF .
-elif [ "$VERSION"V == SF5V ];then
-    echo building for slim-fat sketch version 5
-    cmake -DSF1=OFF -DSF2=OFF -DSF3=OFF -DSF4=OFF -DSF5=ON -DSF6=OFF .
-elif [ "$VERSION"V == SF6V ] || [ "$VERSION"V == V ];then
+    cmake -DSF1=OFF -DSF2=OFF -DSF3=OFF -DSF4=ON -DSFF=OFF .
+elif [ "$VERSION"V == SFFV ] || [ "$VERSION"V == V ];then
     echo building for slim-fat sketch version 6
-    cmake -DSF1=OFF -DSF2=OFF -DSF3=OFF -DSF4=OFF -DSF5=OFF -DSF6=ON .
+    cmake -DSF1=OFF -DSF2=OFF -DSF3=OFF -DSF4=OFF -DSFF=ON .
 else
-    echo "usage:  sh ./generate.sh [SF6|SF1|SF2|SF3|SF4|SF5]"
-    echo "ps: \"sh ./generage.sh\"  for SF6"
+    echo "usage:  sh ./build.sh [SFF|SF1|SF2|SF3|SF4]"
+    echo "ps: \"sh ./build.sh\"  for SFF"
     exit
 fi
 
