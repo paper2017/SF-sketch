907477f14eff2d2302fd5eb37023228125236f82
Paper2017 <paper2017anonym@yahoo.com>
2016-10-20 09:13:55 +0800

    .

--- build.sh
+++ build.sh
@@ -18,13 +18,8 @@ elif [ "$VERSION"V == SFFV ] || [ "$VERSION"V == V ];then
     echo building for slim-fat sketch version 6
     cmake -DSF1=OFF -DSF2=OFF -DSF3=OFF -DSF4=OFF -DSFF=ON .
 else
-<<<<<<< HEAD
-    echo "usage:  sh ./generate.sh [SF6|SF1|SF2|SF3|SF4|SF5]"
-    echo "ps: \"sh ./build.sh\"  for SF6"
-=======
     echo "usage:  sh ./build.sh [SFF|SF1|SF2|SF3|SF4]"
     echo "ps: \"sh ./build.sh\"  for SFF"
->>>>>>> 438ab5785deba7eca59089ef6863e8b99cecb6c6
     exit
 fi
 
